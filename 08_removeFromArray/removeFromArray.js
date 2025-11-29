31b383d7ef09852f1972aa3826888dba9db710c7
KevFroo <193817275+KevFroo@users.noreply.github.com>
2025-11-29 10:36:54 -0600

    Complete 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for(let arg of args) {
+        let index = arr.findIndex((item) => item === arg);
+        while(index !== -1) {
+            arr.splice(index, 1);
+            index = arr.findIndex((item) => item === arg);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
